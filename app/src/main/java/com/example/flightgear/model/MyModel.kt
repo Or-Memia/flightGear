a6d2e3144c59c47f2967aca7a423576118580f55
OR MEMIA <or.memia@live.biu.ac.il>
2021-06-25 02:14:02 +0300

    last changes

--- app/src/main/java/com/example/flightgear/model/MyModel.kt
+++ app/src/main/java/com/example/flightgear/model/MyModel.kt
@@ -27,10 +27,11 @@ class MyModel {
     }
 
     suspend fun send(resolvedCommand: String) {
-        Log.i(TAG,"received $resolvedCommand")
-        withContext(Dispatchers.IO){
+        Log.i(TAG, "received $resolvedCommand")
+        withContext(Dispatchers.IO) {
             out?.let {
-            it.println(resolvedCommand)
+                it.print("${resolvedCommand}\r\n")
+                it.flush()
             }
         }
     }
