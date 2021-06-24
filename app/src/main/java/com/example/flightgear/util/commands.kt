44f660204dd78de0f6827cbc46db17b3b8ac7f51
OR MEMIA <or.memia@live.biu.ac.il>
2021-06-24 23:37:55 +0300

    last changes

--- app/src/main/java/com/example/flightgear/util/commands.kt
+++ app/src/main/java/com/example/flightgear/util/commands.kt
@@ -4,10 +4,17 @@ const val base = "set /controls/flight"
 const val engine = "set /controls/flight/current-engine/throttle"
 
 sealed class Commands(open val cmd: String, open val value: Double) {
-    data class Aileron(override val cmd: String = "$base/aileron", override val value: Double) : Commands(cmd, value)
-    data class Elevator(override val cmd: String = "$base/elevator", override val value: Double) : Commands(cmd, value)
-    data class Rudder(override val cmd: String = "$base/rudder", override val value: Double) : Commands(cmd, value)
-    data class Engine(override val cmd: String = "$base/engine", override val value: Double) : Commands(cmd, value)
+    data class Aileron(override val cmd: String = "$base/aileron", override val value: Double) :
+        Commands(cmd, value)
+
+    data class Elevator(override val cmd: String = "$base/elevator", override val value: Double) :
+        Commands(cmd, value)
+
+    data class Rudder(override val cmd: String = "$base/rudder", override val value: Double) :
+        Commands(cmd, value)
+
+    data class Engine(override val cmd: String = "$base/engine", override val value: Double) :
+        Commands(cmd, value)
 
     val rCommand: String
         get() = "$cmd $value"
