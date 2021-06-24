afd80cc6fbe96cefcac61c8d192cd049049987b7
OR MEMIA <or.memia@live.biu.ac.il>
2021-06-24 22:44:14 +0300

    last changes

--- app/src/main/java/com/example/flightgear/view/MainActivity.kt
+++ app/src/main/java/com/example/flightgear/view/MainActivity.kt
@@ -7,7 +7,9 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.flightgear.R
+import com.example.flightgear.databinding.ActivityMainBinding
 import com.example.flightgear.util.Commands
 import com.example.flightgear.viewModel.MyViewModel
 import com.google.android.material.slider.Slider
@@ -17,18 +19,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         val vm: MyViewModel by viewModels()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main)
+        binding.viewmodel = vm
 
-        // get reference to button
-        val btnConnetion = findViewById<Button>(R.id.button)
-        // set on-click listener
-        btnConnetion.setOnClickListener {
-            vm.setConnection()
-            val rudder = findViewById<Slider>(R.id.rudder)
-            rudder.addOnChangeListener { _, amount, _ ->
-                Log.i(":::MainActivity", "rudder = $amount / ${(amount.toInt() * 0.01)}")
-                vm.sendCommand(Commands.Rudder(value = (amount.toInt().toLong()/100)))
-            }
-        }
+
+
+
+//        setContentView(R.layout.activity_main)
+//
+//         get reference to button
+//        val btnConnetion = findViewById<Button>(R.id.button)
+//         set on-click listener
+//        btnConnetion.setOnClickListener {
+//            vm.setConnection()
+//            val rudder = findViewById<Slider>(R.id.rudder)
+//            rudder.addOnChangeListener { _, amount, _ ->
+//                Log.i(":::MainActivity", "rudder = $amount / ${(amount.toInt() * 0.01)}")
+//                vm.sendCommand(Commands.Rudder(value = (amount.toInt().toLong()/100)))
+//            }
+//        }
     }
 }
