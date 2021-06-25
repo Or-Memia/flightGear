e474eb0fd625529128c27355b142f4cc2882b67a
OR MEMIA <or.memia@live.biu.ac.il>
2021-06-25 03:24:21 +0300

    last changes

---
+++ app/src/main/java/com/example/flightgear/view/JoystickView.kt
@@ -0,0 +1,65 @@
+package com.example.flightgear.view
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.PointF
+import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+
+class JoystickView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+    val p = Paint().apply {
+        color = Color.BLACK
+        isAntiAlias = true
+        style = Paint.Style.FILL
+    }
+
+    var circleRadius = 0f
+    var center = PointF()
+    var defaultCenter = PointF()
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        Log.i(":::JoystickView", "@onSizeChanged -> called wit w = $w h = $h")
+        super.onSizeChanged(w, h, oldw, oldh)
+        circleRadius = minOf(w, h) / 4.0f
+        center = PointF(w / 2.0f, h / 2.0f)
+        defaultCenter = PointF(w / 2.0f, h / 2.0f)
+        invalidate()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        canvas?.drawCircle(center.x, center.y, circleRadius, p)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        event?.let { e ->
+            when (e.action) {
+                MotionEvent.ACTION_MOVE -> updateView(e.x, e.y)
+                MotionEvent.ACTION_UP -> resetView()
+                else -> {
+                    /*ignore*/
+                }
+            }
+            return true
+        } ?: return true
+    }
+
+    private fun resetView() {
+        center = defaultCenter
+        invalidate()
+    }
+
+    private fun updateView(x: Float, y: Float) {
+        center = PointF(x, y)
+        invalidate()
+    }
+
+
+
+}
